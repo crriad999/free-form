2d0a82d451042bacbfb01a3866b7cda12f6c8898
crriad999 <riad.t@uttarauniversity.edu.bd>
2025-12-26 10:40:26 +0600

    this first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+
+  let a = 'justise for';
+  let c = 'PATRONUS :'
+  
+  return (
+    <>
+     <h1 className="text-red-700">we are {a} Hadi</h1>
+     <p>GRAND OPENING {c}</p>
+    </>
+  )
+}
+
+export default App
